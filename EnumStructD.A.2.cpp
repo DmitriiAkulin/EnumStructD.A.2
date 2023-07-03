4dea31c43652322139faf3aa38b9def94dcd09a5
Dmitrii Akulin <dimaakulin@yandex.ru>
2023-07-03 12:36:05 +0300

    initial commit

---
+++ EnumStructD.A.2.cpp
@@ -0,0 +1,42 @@
+﻿// EnumStructD.A.2
+#include <Windows.h>
+#include <iostream>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct BankAccount {
+ int account_num;
+ string name;
+ double balance;
+ };
+
+ void change_balance(BankAccount &dat);//прототип функции изменения счета
+
+int main()
+{
+    
+    setlocale(LC_ALL,"Russian");//тут работает только вывод русского в кавычках  типа "Дима"
+    //настраиваем русский язык для ввода-вывода:
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+
+    BankAccount data;
+    cout << "Введите номер счета: "; cin >> data.account_num;
+    cout << "Введите имя владельца: "; cin >> data.name;
+    cout << "Введите баланс счета: "; cin >> data.balance;
+
+    change_balance(data);
+
+    cout << "Ваш счёт: ";std::cout << data.name << ", " << data.account_num << ", " << data.balance << endl;
+
+    return 0;
+}
+//функция релиз
+void change_balance(BankAccount &dat) {
+    cout << "Введите новый баланс: ";
+    cin >> dat.balance;
+}
+
